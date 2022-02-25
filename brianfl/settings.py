f6aa8d2f6c49584f517a2dbfc336f489d341739c
j <brianjflaherty@pm.me>
2022-02-25 18:03:17 +0000

    Changing and removing secret key.

--- brianfl/settings.py
+++ brianfl/settings.py
@@ -20,7 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1!gy-@%zz=&78&f%jux-d_%60(ck&&(hkbpw)gnip^mdirmi-%'
+with open("/home/j/django_secret.txt", "r") as secret_file:
+    my_secret_key = secret_file.read().strip()
+
+SECRET_KEY = my_secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
